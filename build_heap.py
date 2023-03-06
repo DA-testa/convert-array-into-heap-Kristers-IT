5b6310c2835c4bdd75119070a2b48b54f82cf9c9
Kristers Vilks <112978132+Kristers-IT@users.noreply.github.com>
2023-03-06 19:57:08 +0000

    6marts

--- build_heap.py
+++ build_heap.py
@@ -1,8 +1,26 @@
 # python3
+import numpy as np
 
 
-def build_heap(data):
+def build_heap(data,n):
     swaps = []
+
+    pedejais_elements = n // 2
+    while pedejais_elements >= 0:
+        j = pedejais_elements
+        if 2*pedejais_elements+1 <= n-1 and data[j] < data[2*pedejais_elements+1]:
+            j = 2*pedejais_elements+1
+            data[pedejais_elements], data[2*pedejais_elements + 1] = data[2*pedejais_elements + 1], data[pedejais_elements]
+            if j != pedejais_elements:
+                swaps.append((pedejais_elements,j))
+        if 2*pedejais_elements+2 <= pedejais_elements -1 and data[j] < data[2*pedejais_elements+1]:
+            data[pedejais_elements], data[2*pedejais_elements + 1] = data[2*pedejais_elements + 1], data[pedejais_elements]
+            j = 2*pedejais_elements+2
+            data[pedejais_elements], data[2*pedejais_elements + 2] = data[2*pedejais_elements + 2], data[pedejais_elements]
+            if j != pedejais_elements:
+                swaps.append((pedejais_elements,j))
+        
+        pedejais_elements = pedejais_elements - 1
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
 
@@ -16,17 +34,25 @@ def main():
     # add another input for I or F 
     # first two tests are from keyboard, third test is from a file
 
+    text = input()
 
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
+    if text.startswith('I'):
 
+        n = int(input())
+        data = list(map(int, input().split()))
+        
+    elif text.startswith('F'):
+        nosaukums = input("Ievadi faila nosaukumu: ") 
+        fails = open("./test/" + nosaukums, "r")
+        n = int(fails.readline())
+        data = np.asarray(list(map(int,fails.readline().split())))
+    
     # checks if lenght of data is the same as the said lenght
     assert len(data) == n
 
     # calls function to assess the data 
     # and give back all swaps
-    swaps = build_heap(data)
+    swaps = build_heap(data,n)
 
     # TODO: output how many swaps were made, 
     # this number should be less than 4n (less than 4*len(data))
@@ -34,8 +60,8 @@ def main():
 
     # output all swaps
     print(len(swaps))
-    for i, j in swaps:
-        print(i, j)
+    for pedejais_elements, j in swaps:
+        print(pedejais_elements, j)
 
 
 if __name__ == "__main__":

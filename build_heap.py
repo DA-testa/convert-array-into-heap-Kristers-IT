dc0199b5a2950fa32edab8b75b0a963554923de4
Kristers Vilks <112978132+Kristers-IT@users.noreply.github.com>
2023-03-07 19:23:28 +0000

    7marts

--- build_heap.py
+++ build_heap.py
@@ -4,40 +4,46 @@ import numpy as np
 
 def build_heap(data,n):
     swaps = []
-
-    pedejais_elements = n // 2
-    while pedejais_elements >= 0:
+    for pedejais_elements in range (n // 2, -1, -1):
         j = pedejais_elements
-        if 2*pedejais_elements+1 <= n-1 and data[j] < data[2*pedejais_elements+1]:
-            j = 2*pedejais_elements+1
-            data[pedejais_elements], data[2*pedejais_elements + 1] = data[2*pedejais_elements + 1], data[pedejais_elements]
-            if j != pedejais_elements:
-                swaps.append((pedejais_elements,j))
-        if 2*pedejais_elements+2 <= pedejais_elements -1 and data[j] < data[2*pedejais_elements+1]:
-            data[pedejais_elements], data[2*pedejais_elements + 1] = data[2*pedejais_elements + 1], data[pedejais_elements]
-            j = 2*pedejais_elements+2
-            data[pedejais_elements], data[2*pedejais_elements + 2] = data[2*pedejais_elements + 2], data[pedejais_elements]
-            if j != pedejais_elements:
-                swaps.append((pedejais_elements,j))
-        
-        pedejais_elements = pedejais_elements - 1
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
 
+        if 2*pedejais_elements+1 < n and data[2*pedejais_elements+1] < data[j]:
+            j = 2*pedejais_elements+1
+
+        if 2*pedejais_elements+2 < n and data[2*pedejais_elements+2] < data[j]:
+            j = 2*pedejais_elements+2
+            
+        if j != pedejais_elements:
+            swaps.append((pedejais_elements,j))
+            data[pedejais_elements], data[j] = data[j], data[pedejais_elements]
+            pedejais_elements = j
+        
+            i = j
+            
+            while  i < n // 2:
+                j = i
+
+                if 2*i+1 < n and data[2*i+1] < data[j]:
+                    j = 2*i+1
+
+                if 2*i+2 < n and data[2*i+2] < data[j]:
+                    j = 2*i+2
+                
+                if j != i:
+                    swaps.append((j, i))
+                    data[i], data[j] = data[j], data[i]
+                    i = j
+
+                else:
+                    break
 
     return swaps
 
-
 def main():
     
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
-
     text = input()
 
     if text.startswith('I'):
-
         n = int(input())
         data = list(map(int, input().split()))
         
@@ -47,22 +53,13 @@ def main():
         n = int(fails.readline())
         data = np.asarray(list(map(int,fails.readline().split())))
     
-    # checks if lenght of data is the same as the said lenght
     assert len(data) == n
 
-    # calls function to assess the data 
-    # and give back all swaps
-    swaps = build_heap(data,n)
+    swaps = build_heap(data, n)
 
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
     print(len(swaps))
-    for pedejais_elements, j in swaps:
-        print(pedejais_elements, j)
-
+    for i, j in swaps:
+        print(i, j)
 
 if __name__ == "__main__":
     main()

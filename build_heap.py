c2130a30732c8db1cd0a2787d7ebd906d2450f9f
Kristers Vilks <112978132+Kristers-IT@users.noreply.github.com>
2023-03-09 18:01:58 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,68 +1,66 @@
 # python3
 
-def build_heap(data,n):
+import math
+
+def build_heap(n,data):
     swaps = []
-    for pedejais_elements in range (n // 2, -1, -1):
-        j = pedejais_elements
-        
-        if 2*pedejais_elements+1 < n and data[2*pedejais_elements+1] < data[j]:
-            j = 2*pedejais_elements+1
-
-        if 2*pedejais_elements+2 < n and data[2*pedejais_elements+2] < data[j]:
-            j = 2*pedejais_elements+2
+    n = n-1
+    for indekss in range(n, 0, -1):
+        berns = indekss
+        vecaks = math.floor((indekss-1)/2)
+        for indekss in range (n):
+            if vecaks >= 0 and data[berns] < data[vecaks]:
+                vertiba = data[berns]
+                data[berns] = data[vecaks]
+                data[vecaks] = vertiba
+                swaps.append((vecaks, berns))
+            else:
+                break
             
-        if j != pedejais_elements:
-            swaps.append((pedejais_elements,j))
-            data[pedejais_elements], data[j] = data[j], data[pedejais_elements]
-            pedejais_elements = j
-        
-            i = j
-            
-            while  i < n // 2:
-                j = i
+            berns = vecaks 
+            vecaks = math.floor((berns-1)/2)
 
-                if 2*i+1 < n and data[2*i+1] < data[j]:
-                    j = 2*i+1
+           # while  i < n / 2:
+               # j = i
 
-                if 2*i+2 < n and data[2*i+2] < data[j]:
-                    j = 2*i+2
+               # if 2*i+1 < n and data[2*i+1] < data[j]:
+                   # j = 2*i+1
+
+               # if 2*i+2 < n and data[2*i+2] < data[j]:
+                   # j = 2*i+2
                 
-                if j != i:
-                    swaps.append((j, i))
-                    data[i], data[j] = data[j], data[i]
-                    i = j
+               # if j != i:
+                   # swaps.append((j, i))
+                   # data[i], data[j] = data[j], data[i]
+                   # i = j
 
-                else:
-                    break
+               # else:
+                   # break
 
     return swaps
 
 def main():
     
-    text = input()
+    text = input("Režīms:")
 
     if text.startswith('I'):
         n = int(input())
         data = list(map(int, input().split()))
         
     elif text.startswith('F'):
-        nosaukums = input("Ievadi faila nosaukumu: ") 
+        nosaukums = input("Faila nosaukumu: ") 
         fails = open("./tests/" + nosaukums, "r")
         n = int(fails.readline())
         data = list(map(int,fails.readline().split()))
     
     assert len(data) == n
+    swaps = build_heap(n,data)
 
-    swaps = build_heap(data, n)
-
-    if len(swaps)>4:
-        print(9990)
-    else:
-        print(len(swaps))
-
-    if len(swaps)<4:
-        for i, j in swaps:
-            print(i,j)
+    
+    print(len(swaps))
+    for i, j in swaps:
+        print(i,j)
 
 if __name__ == "__main__":
     main()
+
